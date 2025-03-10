ecbe1f5fdc394ed3c80912c51f75dac1971c4d09
Aldea-Andrei <aldeaandrei1999@yahoo.com>
2025-03-10 23:57:41 +0200

    Fix import

--- app/api/games/[id]/route.js
+++ app/api/games/[id]/route.js
@@ -1,17 +1,10 @@
-import { query } from "@/lib/db";
 import { NextResponse } from "next/server";
 
 export async function GET() {
   try {
-    const { rows: games } = await query("SELECT * FROM games");
-    const { rows: rounds } = await query("SELECT * FROM rounds");
-    const { rows: sets } = await query("SELECT * FROM sets");
-    const { rows: tournaments } = await query("SELECT * FROM tournaments");
-    const { rows: players } = await query("SELECT * FROM players");
 
-    console.log({ games, rounds, sets, tournaments, players });
 
-    return NextResponse.json({ games }, { status: 200 });
+    return NextResponse.json({ "null" }, { status: 200 });
   } catch (error) {
     console.error("Error fetching data:", error);
     return NextResponse.json(
