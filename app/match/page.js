3b53011ef854c4d61d15f3b15d68fb70ccbfc485
Aldea-Andrei <aldeaandrei1999@yahoo.com>
2025-03-11 00:26:07 +0200

    Deprecate page

--- app/match/page.js
+++ app/match/page.js
@@ -14,34 +14,35 @@ export default function MatchPage() {
   const handleClose = () => setModalOpen(false);
 
   return (
-    <div className="p-2">
-      <section className="my-5">
-        <div className="flex gap-2 justify-center items-center">
-          <NavigationButton path="/leaderboard" text="" />
-          <AddRoundButton />
-          <SimpleButton onClick={() => handleOpen()}>
-            <div>
-              <CachedIcon /> Joc Nou
-            </div>
-          </SimpleButton>
-        </div>
-      </section>
-      <section className="flex flex-col gap-2">
-        <GameList />
-      </section>
-      <Modal open={isModalOpen} onClose={handleClose}>
-        <div className="fixed inset-0 flex items-center justify-center p-4">
-          <div className="flex flex-col justify-center items-center text-center gap-5 bg-[#1e2021] border-2 border-black shadow-lg p-4 w-96 rounded-lg">
-            <h1 className="font-semibold text-2xl">
-              Toate datele curente vor fi sterse!
-            </h1>
-            <div className="flex justify-center items-center gap-2">
-              <NavigationButton path="/start" text="Continua" noIcon={true} />
-              <SimpleButton text={"Renunta"} onClick={() => handleClose()} />
-            </div>
-          </div>
-        </div>
-      </Modal>
-    </div>
+    <div>DEPRECATED</div>
+    // <div className="p-2">
+    //   <section className="my-5">
+    //     <div className="flex gap-2 justify-center items-center">
+    //       <NavigationButton path="/leaderboard" text="" />
+    //       <AddRoundButton />
+    //       <SimpleButton onClick={() => handleOpen()}>
+    //         <div>
+    //           <CachedIcon /> Joc Nou
+    //         </div>
+    //       </SimpleButton>
+    //     </div>
+    //   </section>
+    //   <section className="flex flex-col gap-2">
+    //     <GameList />
+    //   </section>
+    //   <Modal open={isModalOpen} onClose={handleClose}>
+    //     <div className="fixed inset-0 flex items-center justify-center p-4">
+    //       <div className="flex flex-col justify-center items-center text-center gap-5 bg-[#1e2021] border-2 border-black shadow-lg p-4 w-96 rounded-lg">
+    //         <h1 className="font-semibold text-2xl">
+    //           Toate datele curente vor fi sterse!
+    //         </h1>
+    //         <div className="flex justify-center items-center gap-2">
+    //           <NavigationButton path="/start" text="Continua" noIcon={true} />
+    //           <SimpleButton text={"Renunta"} onClick={() => handleClose()} />
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </Modal>
+    // </div>
   );
 }
