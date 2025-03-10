07ada1984ca84460ea288e7f07db8d849e201c83
Aldea-Andrei <aldeaandrei1999@yahoo.com>
2025-03-11 00:13:08 +0200

    Fix leaderboard

--- app/api/tournaments/[id]/leaderboard/route.js
+++ app/api/tournaments/[id]/leaderboard/route.js
@@ -4,22 +4,38 @@ import sql from "@/db";
 export async function GET(request, { params }) {
   const { id } = await params;
   try {
-    const playersData =
-      await sql`select distinct players.id, players.name from players
-        join rounds on players.id = rounds.player_1_id OR players.id = rounds.player_2_id
-        join games on rounds.game_id = games.id
-        where games.tournament_id = ${id}`;
+    // const playersData =
+    //   await sql`select distinct players.id, players.name from players
+    //     join rounds on players.id = rounds.player_1_id OR players.id = rounds.player_2_id
+    //     join games on rounds.game_id = games.id
+    //     where games.tournament_id = ${id}`;
 
     const setsData =
-      await sql`select player_1_score, player_1_id, player_2_score, player_2_id, game_id, round_id from sets
+      await sql`select player_1_name, player_1_score, player_1_id, player_2_name, player_2_score, player_2_id, game_id, round_id from sets
         join rounds on rounds.id = sets.round_id
         join games on rounds.game_id = games.id
         where games.tournament_id = ${id}`;
 
-    const players = playersData.reduce((map, player) => {
-      map[player.id] = player.name;
-      return map;
-    }, {});
+    // console.log(playersData);
+    // console.log(
+    //   setsData
+    //     .map((data) => [
+    //       { id: data.player_1_id, name: data.player_1_name },
+    //       { id: data.player_2_id, name: data.player_2_name },
+    //     ])
+    //     .flat()
+    // );
+
+    const players = setsData
+      .map((data) => [
+        { id: data.player_1_id, name: data.player_1_name },
+        { id: data.player_2_id, name: data.player_2_name },
+      ])
+      .flat()
+      .reduce((map, player) => {
+        map[`${player.name}-${player.id}`] = player.name;
+        return map;
+      }, {});
 
     let leaderboard = {};
 
@@ -36,7 +52,7 @@ export async function GET(request, { params }) {
       }`;
       if (!leaderboard[k1])
         leaderboard[k1] = {
-          name: players[set.player_1_id],
+          name: players[k1],
           id: set.player_1_id,
           rounds: [],
           total: 0,
@@ -44,7 +60,7 @@ export async function GET(request, { params }) {
 
       if (!leaderboard[k2])
         leaderboard[k2] = {
-          name: players[set.player_2_id],
+          name: players[k2],
           id: set.player_2_id,
           rounds: [],
           total: 0,
@@ -68,6 +84,13 @@ export async function GET(request, { params }) {
       ).length;
 
       response.push(leaderboard[player]);
+    });
+
+    response.sort((a, b) => {
+      if (a.rounds !== b.rounds) {
+        return b.rounds - a.rounds;
+      }
+      return b.total - a.total;
     });
 
     return NextResponse.json({ leaderboard: response }, { status: 200 });
