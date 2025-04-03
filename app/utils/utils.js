b4c0118a19ca2a94fdcaedf8e4bf1d08a6c7feed
Aldea-Andrei <aldeaandrei1999@yahoo.com>
2025-04-03 16:21:42 +0300

    Fix players order

--- app/utils/utils.js
+++ app/utils/utils.js
@@ -87,7 +87,7 @@ const createMultipleSetsEntry = async (sets) => {
 
 export const generateNewTournament = async () => {
   const storedPlayers = localStorage.getItem("players");
-  const gamePlayers = storedPlayers ? JSON.parse(storedPlayers) : [];
+  let gamePlayers = storedPlayers ? JSON.parse(storedPlayers) : [];
 
   if (gamePlayers.length < 2) return { id: null, error: "Not enough players" };
 
@@ -101,33 +101,61 @@ export const generateNewTournament = async () => {
   let setsData = [];
   let pairs = {};
 
-  gamePlayers
-    .sort(() => Math.random() - 0.5)
-    .forEach((player1) => {
-      gamePlayers
-        .sort(() => Math.random() - 0.5)
-        .forEach((player2) => {
-          const pairKey = `${player1.id}-${player2.id}`;
-          const reversePairKey = `${player2.id}-${player1.id}`;
+  const generateRoundForPair = (player1, player2, pairKey, reversePairKey) => {
+    const round = {
+      player_1_name: player1.name,
+      player_2_name: player2.name,
+      player_1_id: player1.database_id,
+      player_2_id: player2.database_id,
+      game_id: game[0].id,
+    };
 
-          if (player1.id !== player2.id) {
-            if (!pairs[pairKey] && !pairs[reversePairKey]) {
-              const round = {
-                player_1_name: player1.name,
-                player_2_name: player2.name,
-                player_1_id: player1.database_id,
-                player_2_id: player2.database_id,
-                game_id: game[0].id,
-              };
+    pairs[pairKey] = true;
+    pairs[reversePairKey] = true;
 
-              pairs[pairKey] = true;
-              pairs[reversePairKey] = true;
+    roundsData.push(round);
+  };
 
-              roundsData.push(round);
+  let generatedPairs = getPlayersPairs(gamePlayers.length);
+  gamePlayers = gamePlayers.sort(() => Math.random() - 0.5);
+
+  if (generatedPairs.length === 0) {
+    let newGeneratedPairs = [];
+    gamePlayers
+      .sort(() => Math.random() - 0.5)
+      .forEach((player1) => {
+        gamePlayers
+          .sort(() => Math.random() - 0.5)
+          .forEach((player2) => {
+            const pairKey = `${player1.id}-${player2.id}`;
+            const reversePairKey = `${player2.id}-${player1.id}`;
+
+            if (
+              player1.id !== player2.id &&
+              !pairs[pairKey] &&
+              !pairs[reversePairKey]
+            ) {
+              newGeneratedPairs.push([player1.id, player2.id]);
             }
-          }
-        });
-    });
+          });
+      });
+  }
+
+  generatedPairs.forEach((pair) => {
+    const player1 = gamePlayers[pair[0]];
+    const player2 = gamePlayers[pair[1]];
+
+    const pairKey = `${player1.id}-${player2.id}`;
+    const reversePairKey = `${player2.id}-${player1.id}`;
+
+    if (
+      player1.id !== player2.id &&
+      !pairs[pairKey] &&
+      !pairs[reversePairKey]
+    ) {
+      generateRoundForPair(player1, player2, pairKey, reversePairKey);
+    }
+  });
 
   let rounds = await createMultipleRoundsEntry(roundsData);
 
@@ -150,7 +178,7 @@ export const generateNewTournament = async () => {
 
 export const addGameToTournament = async () => {
   const storedPlayers = localStorage.getItem("players");
-  const gamePlayers = storedPlayers ? JSON.parse(storedPlayers) : [];
+  let gamePlayers = storedPlayers ? JSON.parse(storedPlayers) : [];
 
   if (gamePlayers.length < 2) return { id: null, error: "Not enough players" };
 
@@ -170,33 +198,61 @@ export const addGameToTournament = async () => {
   let setsData = [];
   let pairs = {};
 
-  gamePlayers
-    .sort(() => Math.random() - 0.5)
-    .forEach((player1) => {
-      gamePlayers
-        .sort(() => Math.random() - 0.5)
-        .forEach((player2) => {
-          const pairKey = `${player1.id}-${player2.id}`;
-          const reversePairKey = `${player2.id}-${player1.id}`;
+  const generateRoundForPair = (player1, player2, pairKey, reversePairKey) => {
+    const round = {
+      player_1_name: player1.name,
+      player_2_name: player2.name,
+      player_1_id: player1.database_id,
+      player_2_id: player2.database_id,
+      game_id: game[0].id,
+    };
 
-          if (player1.id !== player2.id) {
-            if (!pairs[pairKey] && !pairs[reversePairKey]) {
-              const round = {
-                player_1_name: player1.name,
-                player_2_name: player2.name,
-                player_1_id: player1.database_id,
-                player_2_id: player2.database_id,
-                game_id: game[0].id,
-              };
+    pairs[pairKey] = true;
+    pairs[reversePairKey] = true;
 
-              pairs[pairKey] = true;
-              pairs[reversePairKey] = true;
+    roundsData.push(round);
+  };
 
-              roundsData.push(round);
+  let generatedPairs = getPlayersPairs(gamePlayers.length);
+  gamePlayers = gamePlayers.sort(() => Math.random() - 0.5);
+
+  if (generatedPairs.length === 0) {
+    let newGeneratedPairs = [];
+    gamePlayers
+      .sort(() => Math.random() - 0.5)
+      .forEach((player1) => {
+        gamePlayers
+          .sort(() => Math.random() - 0.5)
+          .forEach((player2) => {
+            const pairKey = `${player1.id}-${player2.id}`;
+            const reversePairKey = `${player2.id}-${player1.id}`;
+
+            if (
+              player1.id !== player2.id &&
+              !pairs[pairKey] &&
+              !pairs[reversePairKey]
+            ) {
+              newGeneratedPairs.push([player1.id, player2.id]);
             }
-          }
-        });
-    });
+          });
+      });
+  }
+
+  generatedPairs.forEach((pair) => {
+    const player1 = gamePlayers[pair[0]];
+    const player2 = gamePlayers[pair[1]];
+
+    const pairKey = `${player1.id}-${player2.id}`;
+    const reversePairKey = `${player2.id}-${player1.id}`;
+
+    if (
+      player1.id !== player2.id &&
+      !pairs[pairKey] &&
+      !pairs[reversePairKey]
+    ) {
+      generateRoundForPair(player1, player2, pairKey, reversePairKey);
+    }
+  });
 
   let rounds = await createMultipleRoundsEntry(roundsData);
 
@@ -215,5 +271,34 @@ export const addGameToTournament = async () => {
   return { id: currentTournamentId, error: null };
 };
 
-// TODO
-// const getPlayersOrder = (count) => {};
+const getPlayersPairs = (count) => {
+  if (count === 2) return [[0, 1]];
+  if (count === 3)
+    return [
+      [0, 1],
+      [1, 2],
+      [0, 2],
+    ];
+  if (count === 4)
+    return [
+      [0, 1],
+      [2, 3],
+      [3, 1],
+      [1, 2],
+      [0, 3],
+    ];
+  if (count === 5)
+    return [
+      [0, 1],
+      [3, 4],
+      [1, 2],
+      [0, 4],
+      [2, 3],
+      [1, 4],
+      [0, 2],
+      [1, 3],
+      [2, 4],
+      [0, 3],
+    ];
+  return [];
+};
